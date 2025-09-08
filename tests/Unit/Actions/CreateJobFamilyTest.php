38fcec1e40b1329611f72586c3231939bd47cc2e
Regis Freyd <djaiss@users.noreply.github.com>
2025-09-07 20:36:55 -0400

    Update CreateJobFamilyTest.php

--- tests/Unit/Actions/CreateJobFamilyTest.php
+++ tests/Unit/Actions/CreateJobFamilyTest.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Actions\CreateJobFamily;
 use App\Models\Organization;
 use Illuminate\Support\Facades\Queue;
+use Illuminate\Validation\ValidationException;
 
 it('creates a job family', function (): void {
     Queue::fake();
@@ -27,4 +28,17 @@ it('creates a job family', function (): void {
         'organization_id' => $organization->id,
         'name' => 'Sales',
     ]);
+});
+
+it('throws an exception if user is not part of organization', function (): void {
+    $this->expectException(ValidationException::class);
+
+    $user = User::factory()->create();
+    $organization = Organization::factory()->create();
+
+    (new CreateJobFamily(
+        user: $user,
+        organization: $organization,
+        jobFamilyName: 'Sales',
+    ))->execute();
 });
